93a6dc77e0bb73156dffa217f557488f7eb0735f
berkslv <berkslv@gmail.com>
2022-12-30 20:07:08 +0300

    Added 167

--- leetcode/167_twoSum.py
+++ leetcode/167_twoSum.py
@@ -1,11 +1,21 @@
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/submissions/868161853/
 from typing import List
+
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
-        return 0
+        l, r = 0, len(numbers)-1
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                return [l+1, r+1]
+            elif s < target:
+                l += 1
+            else:
+                r -= 1
 
-numbers = [2,7,11,15]
-target = 9
+
+numbers = [2, 7, 11, 15]
+target = 26
 result = Solution().twoSum(numbers, target)
 print(" numbers: ", numbers, "\n target: ", target, "\n result: ", result)
