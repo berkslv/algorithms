2ccae32b7afc27465f56baf1a64aaa5d95134ee0
Berk Selvi <berkslv@gmail.com>
2023-04-07 16:24:43 +0300

    Added gradingStudents

---
+++ hackerrank/gradingStudents.java
@@ -0,0 +1,24 @@
+class Result {
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        List<Integer> result = new ArrayList<>();
+        for (int grade : grades) {
+            result.add(evaluate(grade));
+        }
+        return result;
+    }
+
+    public static int evaluate(int grade) {
+        if (grade < 38) {
+            return grade;
+        }
+
+        int nextMultipleOfFive = (grade / 5 + 1) * 5;
+        if (nextMultipleOfFive - grade < 3) {
+            return nextMultipleOfFive;
+        }
+        
+        return grade;
+    }
+
+}
