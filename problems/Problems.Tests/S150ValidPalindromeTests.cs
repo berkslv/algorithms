c3d7cbcedb8ca5a8929cc0607d55eae4e66ff019
Berk Selvi <berk.selvi@d-teknoloji.com.tr>
2025-02-24 14:48:05 +0300

    S150ValidPalindrome added

---
+++ problems/Problems.Tests/S150ValidPalindromeTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Problems.Tests;
+
+public class S150ValidPalindromeTests
+{
+    [Theory]
+    [InlineData("Was it a car or a cat I saw?", true)]
+    [InlineData("tab a cat", false)]
+    [InlineData("A man, a plan, a canal, Panama", true)]
+    [InlineData("No 'x' in Nixon", true)]
+    [InlineData("race a car", false)]
+    [InlineData("", true)]
+    [InlineData(" ", true)]
+    [InlineData("a", true)]
+    [InlineData("ab", false)]
+    [InlineData("!aba", true)]
+    public void IsPalindrome_ShouldReturnExpectedResult(string input, bool expected)
+    {
+        // Arrange
+        var solution = new S150ValidPalindrome();
+
+        // Act
+        var result = solution.IsPalindrome(input);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
---
+++ problems/Problems/S150ValidPalindrome.cs
@@ -0,0 +1,37 @@
+﻿namespace Problems;
+
+/// <summary>
+/// https://neetcode.io/problems/is-palindrome
+/// </summary>
+public class S150ValidPalindrome
+{
+    public bool IsPalindrome(string s)
+    {
+        for (int i = 0, j = s.Length - 1; i < j;)
+        {
+            var left = s[i];
+            if (!char.IsLetterOrDigit(left))
+            {
+                i++;
+                continue;
+            }
+
+            var right = s[j];
+            if (!char.IsLetterOrDigit(right))
+            {
+                j--;
+                continue;
+            }
+
+            if (char.ToLower(left) != char.ToLower(right))
+            {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
